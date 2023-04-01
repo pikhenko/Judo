74a6590fd4a18735726a39a9499e478e12c428f1
Seryonka <seryonka88@gmail.com>
2023-04-01 22:01:03 +0300

    backend folder

--- Judo/Judo/settings.py
+++ Judo/Judo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'backend.apps.BackendConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = []
+
+
---
+++ Judo/backend/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+menu = ["Главная страница", "Фотогалерея", "Расписание", "Наша команда", "Войти"]
+
+def index(request):
+    return render(request, 'backend/index.html', {'menu': menu, 'title': 'Главная страница'})
+
+def photo(request):
+    return render(request, 'backend/photo.html', {'menu': menu, 'title': 'Фотогалерея'})
+
+def team(request):
+    return render(request, 'backend/team.html', {'menu': menu, 'title': 'Наша команда'})
+
+def shedule(request):
+    return render(request, 'backend/shedule.html', {'menu': menu, 'title': 'Расписание'})
