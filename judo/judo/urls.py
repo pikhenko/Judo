8e8f9c614953a669fdea168f22064776926eb6a6
Artem Pikhenko <apikhenko@yandex.ru>
2024-02-01 21:54:57 +0300

    update name judo

--- judo/judo/urls.py
+++ judo/judo/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import include
 from django.urls import path
 
-from Judo import settings
+from judo import settings
 # from backend.views import *
 
 
