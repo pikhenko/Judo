dfb4f11c14801f7257ee0594377f677967aca0d5
Artem Pikhenko <apikhenko@yandex.ru>
2024-02-09 08:35:04 +0300

    udate docker

--- Judo/Judo/urls.py
+++ Judo/Judo/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import include
 from django.urls import path
 
-from judo import settings
+from Judo import settings
 # from backend.views import *
 
 
--- Judo/Judo/wsgi.py
+++ Judo/Judo/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for judo project.
+WSGI config for Judo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'judo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Judo.settings')
 
 application = get_wsgi_application()
